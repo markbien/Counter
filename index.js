f91e45b6b8be45efaef5961fc790a81fe72c5cdd
Mark Bien <48498353+markbien@users.noreply.github.com>
2023-10-25 16:43:54 +0800

    Add function to increase,decrease, and reset displayed number

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 const countDisplay = document.querySelector('.count');
-const decreaseButton = document.querySelector('')
+const decreaseButton = document.querySelector('#decrease');
+const resetButton = document.querySelector('#reset');
+const increaseButton = document.querySelector('#increase');
+
+decreaseButton.addEventListener('click', function(){
+  let currentValue = countDisplay.textContent;
+  currentValue -= 1;
+  countDisplay.textContent = currentValue;
+});
+
+resetButton.addEventListener('click', function(){
+  countDisplay.textContent = '0';
+})
+
+increaseButton.addEventListener('click', function(){
+  let currentValue = countDisplay.textContent;
+  currentValue = parseInt(currentValue) + 1;
+  countDisplay.textContent = currentValue;
+});
