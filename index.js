1c1a0722dc21cb57a99c25dd29a827a87783d599
Mark Bien <48498353+markbien@users.noreply.github.com>
2023-10-25 16:56:42 +0800

    Add function to change color depending on value

--- index.js
+++ index.js
@@ -3,18 +3,33 @@ const decreaseButton = document.querySelector('#decrease');
 const resetButton = document.querySelector('#reset');
 const increaseButton = document.querySelector('#increase');
 
+function changeCountDisplayColor(){
+  const currentValue = parseInt(countDisplay.textContent);
+  const currentColor = countDisplay.style.color;
+  if (currentValue < 0 && currentColor != 'Red') {
+    countDisplay.style.color = 'Red';
+  } else if(currentValue == 0 && currentColor != 'Black') {
+    countDisplay.style.color = 'Black';
+  } else {
+    countDisplay.style.color = "Green";
+  }
+}
+
 decreaseButton.addEventListener('click', function(){
   let currentValue = countDisplay.textContent;
   currentValue -= 1;
   countDisplay.textContent = currentValue;
+  changeCountDisplayColor();
 });
 
 resetButton.addEventListener('click', function(){
   countDisplay.textContent = '0';
+  changeCountDisplayColor();
 })
 
 increaseButton.addEventListener('click', function(){
   let currentValue = countDisplay.textContent;
   currentValue = parseInt(currentValue) + 1;
   countDisplay.textContent = currentValue;
+  changeCountDisplayColor();
 });
